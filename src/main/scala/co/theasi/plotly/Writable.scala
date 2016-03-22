a402b54f2f344327cd41302082692a1bc2365e99
Pascal Bugnion <pascal@bugnion.org>
2016-03-22 10:06:40 +0000

    Add tests for scatter plots.

--- src/main/scala/co/theasi/plotly/Writable.scala
+++ src/main/scala/co/theasi/plotly/Writable.scala
@@ -5,10 +5,10 @@ trait Writable[T] {
 }
 
 trait WritableImplicits {
-  implicit object WritableFloat extends Writable[Float] {
-    def toPType(x: Float) = PDouble(x.toDouble)
-  }
   implicit object WritableDouble extends Writable[Double] {
     def toPType(x: Double) = PDouble(x)
+  }
+  implicit object WritableInt extends Writable[Int] {
+    def toPType(x: Int) = PInt(x)
   }
 }
---
+++ src/test/scala/co/theasi/plotly/writer/PlotWriterSpec.scala
@@ -0,0 +1,61 @@
+package co.theasi.plotly.writer
+
+import org.json4s._
+import org.json4s.native.JsonMethods._
+
+import org.scalatest._
+
+import co.theasi.plotly._
+
+class PlotWriterSpec extends FlatSpec with Matchers {
+
+  /*implicit val testServer = new Server {
+    override val credentials = Credentials("PlotlyImageTest", "786r5mecv0")
+    override val url = "https://api.plot.ly/v2"
+  }*/
+  val testX1 = Vector(1.0, 2.0, 3.0)
+  val testX2 = Vector(1, 2, 3)
+  val testY1 = Vector(4.0, 5.0, 7.0)
+
+  def checkTestX1(arr: JValue) = {
+    val JArray(response) = arr
+    response.toVector shouldEqual testX1.map { JDouble }
+  }
+
+  def checkTestX2(arr: JValue) = {
+    val JArray(response) = arr
+    response.toVector shouldEqual testX2.map { JInt(_) }
+  }
+
+  def checkTestY1(arr: JValue) = {
+    val JArray(response) = arr
+    response.toVector shouldEqual testY1.map { JDouble }
+  }
+
+  def getJsonForPlotFile(plotFile: PlotFile): JValue = {
+    val endPoint = s"plots/${plotFile.fileId}/content"
+    val req = Api.get(endPoint, Seq("inline_data" -> "true"))
+    Api.despatchAndInterpret(req)
+  }
+
+  "draw" should "draw a basic scatter plot" in {
+    val p = Plot().withScatter(testX1, testY1)
+    val plotFile = PlotWriter.draw(p, "test-123")
+
+    // Verify that the plot is correct
+    val jsonResponse = getJsonForPlotFile(plotFile)
+    val series = (jsonResponse \ "data")(0)
+    checkTestX1(series \ "x")
+    checkTestY1(series \ "y")
+  }
+
+  it should "draw a scatter plot with mixed Int/Double" in {
+    val p = Plot().withScatter(testX2, testY1)
+    val plotFile = PlotWriter.draw(p, "test-124")
+
+    val jsonResponse = getJsonForPlotFile(plotFile)
+    val series = (jsonResponse \ "data")(0)
+    checkTestX2(series \ "x")
+    checkTestY1(series \ "y")
+  }
+}
