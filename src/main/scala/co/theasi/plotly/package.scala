1c7734b2f85e7ad7db145a736da8da1e7a45be80
Pascal Bugnion <pascal@bugnion.org>
2016-04-11 07:37:40 +0100

    Scaladoc for package and Plot.
    It's a start... A long way to go, though.

--- src/main/scala/co/theasi/plotly/package.scala
+++ src/main/scala/co/theasi/plotly/package.scala
@@ -1,5 +1,33 @@
 package co.theasi
 
+/** ==Scala Plotly client==
+  *
+  * Plotly is a platform for creating and hosting plots. This
+  * library lets you create, modify and delete plots on Plotly.
+  *
+  * Build a plot using [[Plot]], and send that
+  * plot to the Plotly servers with [[draw]].
+  *
+  * {{{
+  * import co.theasi.plotly._
+  *
+  * val xs = Vector(1, 2, 3)
+  * val ys = Vector(4.5, 8.5, 21.0)
+  *
+  * val p = Plot().withScatter(xs, ys)
+  * draw(p, "my-first-plot")
+  * }}}
+  *
+  * ==Useful links==
+  *
+  * For more information on how to build complex plot objects,
+  * read the documentation for [[Plot]].
+  *
+  * To control how the client deals with existing files with
+  * the same name on the Plotly server, and to control the
+  * privacy options, read the documentation
+  * for [[writer.FileOptions]]
+  */
 package object plotly
 extends WritableImplicits
 with ReadableImplicits
