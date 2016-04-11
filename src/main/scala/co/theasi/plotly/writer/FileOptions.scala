96682d6aa382b26fe93f44cd55fe3d08b5a395a7
Pascal Bugnion <pascal@bugnion.org>
2016-04-11 07:50:40 +0100

    Document FileOptions.

--- src/main/scala/co/theasi/plotly/writer/FileOptions.scala
+++ src/main/scala/co/theasi/plotly/writer/FileOptions.scala
@@ -1,3 +1,8 @@
 package co.theasi.plotly.writer
 
+/** Control how an object is written to Plotly.
+  *
+  * @param overwrite If true, silently overwrite an existing file
+  *   with the same name; if false, throws a [[PlotlyException]]
+  */
 case class FileOptions(overwrite: Boolean = true)
