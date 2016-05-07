f9e78fef223904beef022e2af4f746a0bbde9f96
Pascal Bugnion <pascal@bugnion.org>
2016-05-07 16:08:52 +0100

    Ability to draw single plots.
    Previously, the only way to draw a single plot was to construct a
    figure. This is unnecessary boiler-plate if you just want a single plot.
    This commit adds a draw method to draw plots directly.

--- src/main/scala/co/theasi/plotly/writer/ServerWriter.scala
+++ src/main/scala/co/theasi/plotly/writer/ServerWriter.scala
@@ -1,6 +1,6 @@
 package co.theasi.plotly.writer
 
-// import co.theasi.plotly.{Plot, Grid}
+import co.theasi.plotly.{Plot, Grid, Figure, SinglePlotFigure}
 
 trait ServerWriterWithDefaultCredentials extends ServerWriter {
   implicit val server = ServerWithDefaultCredentials
@@ -9,40 +9,56 @@ trait ServerWriterWithDefaultCredentials extends ServerWriter {
 trait ServerWriter {
   implicit val server: Server
 
-  // def draw(
-  //     plot: Plot[_],
-  //     fileName: String)
-  //     (implicit server: Server)
-  // : PlotFile = {
-  //   draw(plot, fileName, FileOptions())
-  // }
+  def draw(
+      figure: Figure,
+      fileName: String)
+      (implicit server: Server)
+  : PlotFile = {
+    draw(figure, fileName, FileOptions())
+  }
 
-  // def draw(
-  //     plot: Plot[_],
-  //     fileName: String,
-  //     fileOptions: FileOptions)
-  //     (implicit server: Server)
-  // : PlotFile = {
-  //   PlotWriter.draw(plot, fileName, fileOptions)
-  // }
+  def draw(
+    figure: Figure,
+    fileName: String,
+    fileOptions: FileOptions)
+    (implicit server: Server)
+  : PlotFile = {
+    FigureWriter.draw(figure, fileName, fileOptions)
+  }
 
-  // def draw(
-  //     grid: Grid,
-  //     fileName: String)
-  //     (implicit server: Server)
-  // : GridFile = {
-  //   draw(grid, fileName, FileOptions())
-  // }
+  def draw(
+      plot: Plot,
+      fileName: String)
+      (implicit server: Server)
+  : PlotFile = {
+    draw(plot, fileName, FileOptions())
+  }
 
-  // def draw(
-  //     grid: Grid,
-  //     fileName: String,
-  //     fileOptions: FileOptions)
-  //     (implicit server: Server)
-  // : GridFile = {
-  //   GridWriter.draw(grid, fileName, fileOptions)
-  // }
+  def draw(
+      plot: Plot,
+      fileName: String,
+      fileOptions: FileOptions)
+      (implicit server: Server)
+  : PlotFile = {
+    val figure = SinglePlotFigure(plot)
+    draw(figure, fileName, fileOptions)
+  }
 
+  def draw(
+      grid: Grid,
+      fileName: String)
+      (implicit server: Server)
+  : GridFile = {
+    draw(grid, fileName, FileOptions())
+  }
 
+  def draw(
+      grid: Grid,
+      fileName: String,
+      fileOptions: FileOptions)
+      (implicit server: Server)
+  : GridFile = {
+    GridWriter.draw(grid, fileName, fileOptions)
+  }
 
 }
