aa66939509ae9b0cb4da7df83cdb87b0b05cffab
Pascal Bugnion <pascal@bugnion.org>
2016-05-15 16:13:44 +0100

    More unused imports

--- src/main/scala/co/theasi/plotly/writer/OptionsWriter.scala
+++ src/main/scala/co/theasi/plotly/writer/OptionsWriter.scala
@@ -1,7 +1,6 @@
 package co.theasi.plotly.writer
 
 import org.json4s._
-import org.json4s.native.JsonMethods._
 import org.json4s.JsonDSL._
 
 import co.theasi.plotly.{ScatterOptions, ScatterMode, MarkerOptions,
