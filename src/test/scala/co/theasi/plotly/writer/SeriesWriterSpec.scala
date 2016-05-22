910077f500323b8b64c82e89d2a7cf859c053b30
Pascal Bugnion <pascal@bugnion.org>
2016-05-22 11:46:20 +0100

    Unittests for writing surface series.

---
+++ src/test/scala/co/theasi/plotly/writer/SeriesWriterSpec.scala
@@ -0,0 +1,22 @@
+package co.theasi.plotly.writer
+
+import org.scalatest._
+
+import org.json4s._
+
+import co.theasi.plotly.SurfaceOptions
+
+class SeriesWriterSpec extends FlatSpec with Matchers {
+
+  "toJson" should "serialize the srcs and scenes for surfaces" in {
+    val srcs = List("src1")
+    val sceneIndex = 2
+    val options = SurfaceOptions()
+    val writeInfo = SurfaceWriteInfo(srcs, sceneIndex, options)
+
+    val jobj = SeriesWriter.toJson(writeInfo)
+    (jobj \ "zsrc") shouldEqual JString("src1")
+    (jobj \ "scene") shouldEqual JString("scene2")
+  }
+
+}
