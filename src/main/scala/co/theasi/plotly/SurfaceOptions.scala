e9e816881717eada177fa6d7141410d5a014f0dd
Pascal Bugnion <pascal@bugnion.org>
2016-06-19 15:27:51 +0100

    Basic documentation for SurfaceOptions.

--- src/main/scala/co/theasi/plotly/SurfaceOptions.scala
+++ src/main/scala/co/theasi/plotly/SurfaceOptions.scala
@@ -6,9 +6,15 @@ case class SurfaceOptions(
   showScale: Option[Boolean],
   colorscale: Option[Colorscale]
 ) extends SeriesOptions {
+
+  /** Set the name of the series */
   def name(newName: String): SurfaceOptions =
     copy(name = Some(newName))
 
+  /** Set the surface opacity.
+    *
+    * @param newOpacity Opacity value; must be between 0 and 1.
+    */
   def opacity(newOpacity: Double): SurfaceOptions = {
     require(
       newOpacity >= 0.0 && newOpacity <= 1.0,
@@ -16,9 +22,16 @@ case class SurfaceOptions(
     copy(opacity = Some(newOpacity))
   }
 
+  /** Draw a color bar on the side of the plot mapping from color to z-value */
   def withScale(): SurfaceOptions = copy(showScale = Some(true))
+
+  /** Hide color bar on the side of the plot mapping from color to z-value */
   def noScale(): SurfaceOptions = copy(showScale = Some(false))
 
+  /** Set the colorscale.
+    *
+    * @param newColorscale Colorscale name
+    */
   def colorscale(newColorscale: String) =
     copy(colorscale = Some(ColorscalePredef(newColorscale)))
 }
